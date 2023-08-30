a4683e00e48847813dfc2db3c57ce90f10b96d5f
Ekradeveloper <95731164+EkradeveloperBD@users.noreply.github.com>
2023-08-30 10:34:02 +0600

    route.js

--- src/app/api/setting/route.js
+++ src/app/api/setting/route.js
@@ -1,11 +1,9 @@
+import { setCookie } from 'next/cookies';
 
-import { NextRequest, NextResponse } from 'next/server';
-import { middleware } from './middleware'; // Import the middleware
+export default function handler(req, res) {
+  setCookie(res, 'theme', 'dark', {
+    sameSite: 'strict'
+  });
 
-export default function handler(req) {
-  // Use the middleware to modify the request
-  const modifiedReq = middleware(req);
-
-  // Use NextResponse.next() to produce the response using the modified request
-  return NextResponse.next(`Modified Authorization Header: ${modifiedReq.headers.get('Authorization')}`);
+  res.status(200).send('Cookie set successfully');
 }
