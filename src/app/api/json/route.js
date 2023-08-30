df5dbdd6b4b62ae313148108ea513bea903b54d6
Ekradeveloper <95731164+EkradeveloperBD@users.noreply.github.com>
2023-08-30 10:30:32 +0600

    route.js

--- src/app/api/json/route.js
+++ src/app/api/json/route.js
@@ -1,11 +1,14 @@
-export default async () => {
-    const jsonData = {
-      message: 'Welcome to our API!',
-      version: '1.0',
-    };
-  
-    return new Response(JSON.stringify(jsonData), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+import { NextRequest } from 'next/server';
+
+export default function handler(req) {
+  const responseData = {
+    message: "Welcome to our API!",
+    version: "1.0"
   };
-  
+
+  return new Response(JSON.stringify(responseData), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
