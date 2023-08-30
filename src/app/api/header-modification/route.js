090476aa4111e4d2e3963b13a96e13fa43aafd4b
Ekradeveloper <95731164+EkradeveloperBD@users.noreply.github.com>
2023-08-30 10:01:26 +0600

    route.js

--- src/app/api/header-modification/route.js
+++ src/app/api/header-modification/route.js
@@ -1,11 +1,20 @@
-export default async (req) => {
-    const authorizationHeader = req.headers.get('Authorization');
-    if (authorizationHeader) {
-      const modifiedHeader = `Bearer ${authorizationHeader}`;
-      const response = new Response(null, { headers: { Authorization: modifiedHeader } });
-      return NextResponse.next(response);
-    }
-    
-    return new Response('No Authorization header found.', { status: 400 });
+import { NextRequest, NextResponse } from 'next/server';
+
+export default function handler(req) {
+  const { headers } = req;
+  const token = headers.get('Authorization').split(' ')[1]; // Assuming "Bearer TOKEN"
+
+  const newAuthorizationHeader = `Bearer ${token}`;
+
+  const responseHeaders = new Headers(headers);
+  responseHeaders.set('Authorization', newAuthorizationHeader);
+
+  const responseData = {
+    message: 'Authorization header modified successfully.'
   };
-  
+
+  return new Response(JSON.stringify(responseData), {
+    headers: responseHeaders,
+    status: 200
+  });
+}
